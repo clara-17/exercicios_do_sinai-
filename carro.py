ab17cd538c1fc302f59a961b3cc764bd88a7df1b
clara-17 <anaclarapereiraalves44@gmail.com>
2025-05-09 15:24:14 -0300

    Add files via upload

---
+++ carro.py
@@ -0,0 +1,6 @@
+carro = input ("fala seu carro dos sonhos: ") 
+print (carro == "bmw m8")
+if carro == "bmw m8":
+    print("o meu tambem")
+else:
+    print("aff que gosto ruim...=/")
---
+++ oi.py
@@ -0,0 +1,6 @@
+
+nome = input("qual é seu nome? ")
+idade = input("qual é sua idade?")
+print("oi",  nome , "sua iadade é" , idade)
+
+      
